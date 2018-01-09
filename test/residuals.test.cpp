250b118bd50dcebe108c7ba18547796324838281
Austin Schneider <hogenshpogen@gmail.com>
2018-01-09 13:09:21 -0600

    Test changes

--- test/residuals.test.cpp
+++ test/residuals.test.cpp
@@ -2,13 +2,16 @@
 #include "../PhysTools/residuals.h"
 
 int main() {
-    std::vector<double> z = {-3., 1., 2., 3.};
-    std::vector<unsigned int> n {1, 3, 3, 1};
-    std::vector<double> s = {0., -1., -2.};
-    std::vector<unsigned int> m = {1, 4, 1};
+    std::vector<double> z = {0};
+    std::vector<unsigned int> n {5};
+    std::vector<double> s = {0.0001, 0.0001001, 0.0001002, 0.0001003};
+    std::vector<unsigned int> m = {1, 1, 1, 1};
     std::vector<double> c = phys_tools::residuals::residual_computer<double>()(z, n, s, m);
+    double c_sum = 0;
     for(unsigned int i=0; i<c.size(); ++i) {
         std::cout << c[i] << std::endl;
+        c_sum += c[i];
     }
+    std::cout << c_sum << std::endl;
     return 0;
 }
